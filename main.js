9deac12c65205039b1cfe7dda9b73bf14f1f5905
fesyafom <a.feschenko@protonmail.com>
2017-06-11 19:13:53 +0300

    refactoring

--- main.js
+++ main.js
@@ -1,18 +1,45 @@
-function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-    setLogo(profile.getImageUrl(), profile.getName())
+let initObjAPI = {};
+
+function start() {
+    gapi.load('client:auth2', initClient)
 }
 
-function signOut() {
+function initClient () {
+    const params = {
+        client_id: '982596103606-3lfc4uhonegobtjvjsgvnvij5j27aml2.apps.googleusercontent.com',
+        fetch_basic_profile: true,
+        scope: "profile email",
+        response_type: "code"
+    };
+
+    gapi.auth2.init(params)
+        .then((initObj) => {
+            initObjAPI = initObj;
+        })
+}
+
+function login() {
+    initObjAPI.signIn()
+        .then((googleUser) => onSignIn(googleUser))
+}
+
+function logout() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
         console.log('User signed out.');
     });
     delLogo()
+}
+
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    var authResp = googleUser.getAuthResponse();
+    console.log('AuthData: ', authResp);
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    setLogo(profile.getImageUrl(), profile.getName())
 }
 
 function setLogo(logoUrl, userName) {
@@ -32,3 +59,4 @@ function delLogo() {
     el.classList.remove("open");
     login.className += " open";
 }
+
